59a3d9288e0f84f542e73a8d95ba05c963bb5256
Czarek98 <cezary.bohdanowicz@gmail.com>
2023-05-02 15:32:14 +0200

    Add API with fixed CORS

---
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            return new List<SuperHero>() 
+            { 
+                new SuperHero 
+                { 
+                    Name = "Batman",
+                    FirstName = "Bruce",
+                    LastName = "Wayne",
+                    Place = "Gotham City",
+                } 
+            };
+        }
+    }
+}
