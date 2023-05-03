2b2278d493277e9d399cd70889bdf1b28bca0cab
Czarek98 <cezary.bohdanowicz@gmail.com>
2023-05-03 13:55:25 +0200

    Delete mock, get data from DB

--- SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -1,25 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Data;
 
 namespace SuperHeroAPI.Controllers
 {
 
-    [ApiController]
-    [Route("api/[controller]")]
-    public class SuperHeroController : ControllerBase
+  [ApiController]
+  [Route("api/[controller]")]
+  public class SuperHeroController : ControllerBase
+  {
+    private readonly DataContext _dbContext;
+
+    public SuperHeroController(DataContext dbContext)
     {
-        [HttpGet]
-        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
-        {
-            return new List<SuperHero>() 
-            { 
-                new SuperHero 
-                { 
-                    Name = "Batman",
-                    FirstName = "Bruce",
-                    LastName = "Wayne",
-                    Place = "Gotham City",
-                } 
-            };
-        }
+      _dbContext = dbContext;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+    {
+      return Ok(await _dbContext.SuperHeroes.ToListAsync());
+    }
+  }
 }
