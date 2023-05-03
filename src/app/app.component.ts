0c3c2c36e1f99bb20fae858db8788527f8cd47a2
Czarek98 <cezary.bohdanowicz@gmail.com>
2023-05-03 15:56:17 +0200

    Add form to manage heroes on DB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { SuperHeroService } from './services/super-hero.service';
 export class AppComponent {
   title = 'HeroAcademy-app';
   heroes: SuperHero[] = [];
+  heroToEdit?: SuperHero;
 
   constructor(private superHeroService : SuperHeroService) {}
 
@@ -17,5 +18,13 @@ export class AppComponent {
     this.superHeroService
     .getSuperHeroes()
     .subscribe((result: SuperHero[]) => (this.heroes = result));
+  }
+
+  initNewHero() {
+    this.heroToEdit = new SuperHero();
+  }
+
+  editHero(hero: SuperHero){
+    this.heroToEdit = hero;
   }
 }
